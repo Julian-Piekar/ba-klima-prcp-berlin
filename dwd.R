60810a67d154601681b47c20233b5dca25a9f17e
Julian-Piekar <124191965+Julian-Piekar@users.noreply.github.com>
2023-02-10 19:35:54 +0100

    Update dwd.R

--- dwd.R
+++ dwd.R
@@ -9,14 +9,3 @@ buch<-read.table("C:/UNI/BA/BA Klima/R Studio/dwd-daten/400_h_0604_0519.csv", he
 buch$MESS_DATUM <- as.POSIXct(as.character(buch$MESS_DATUM),format = "%Y%m%d%H",tz="UTC")
 
 
-pr.a1 <- vector() # empty vector for hourly prcp
-
-for (i in 1:8760) pr.a1[i] <- max(alex[i], na.rm=TRUE) #Error in FUN(X[[i]], ...) : only defined on a data frame with all numeric-alike variables
-options(max.print=8760)
-print("")
-print("Hourly")
-print("---------------------------")
-print(round(pr.a1, digits=1)) # output to console (digits = 1 bedeutet dass die Werte auf eine Stelle nach dem Komma gerundet werden)
-
-plot(1:8760, pr.a1, main="Hourly pr (DWD Alex)",
-     xlab=paste0("Hours ", year), ylab="PRCP mm h-1")
