facdb1a3990f342b4b1ea1bb4d19b28462cd8a64
Julian-Piekar <124191965+Julian-Piekar@users.noreply.github.com>
2023-02-15 11:09:41 +0100

    Update dwd.R

--- dwd.R
+++ dwd.R
@@ -1,35 +1,13 @@
-alex<-read.csv("C:/UNI/BA/BA Klima/R Studio/dwd-daten/399_h_0301_0711.csv", header=TRUE, 
-                 na.strings="NA", dec=".", stringsAsFactors = T, sep = ";")
-alex$MESS_DATUM <- as.POSIXct(as.character(alex$MESS_DATUM),format = "%Y%m%d%H",tz="UTC")
+# BER = 427
 
-pr.a1 <- vector() # empty vector for hourly prcp
+ber <- read.csv("C:/UNI/BA/BA Klima/R Studio/dwd-daten/427_h_0104_1219.csv", header=TRUE, 
+               na.strings="NA", dec=".", stringsAsFactors = T, sep = ";")
+ber$MESS_DATUM <- as.POSIXct(as.character(ber$MESS_DATUM),format = "%Y%m%d%H",tz="UTC")
 
-for (i in 1:8760) pr.a1[i] <- max(alex[i,3], na.rm=FALSE)
-options(max.print=8760)
-print("")
-print("Hourly")
-print("---------------------------")
-print(round(pr.a1, digits=1)) # output to console (digits = 1 bedeutet dass die Werte auf eine Stelle nach dem Komma gerundet werden)
-
-plot(1:8760, pr.a1, main="Hourly pr (CER 2 km domain)",
-     xlab=paste0("Hours "), ylab="PRCP mm h-1")
+minhour <- seq(as.POSIXct("2004-01-01 00:00:00"), as.POSIXct("2004-12-31 23:00:00"), by="hour")
+minhour <- data.frame( MESS_DATUM = minhour)
 
 
-buch<-read.csv("C:/UNI/BA/BA Klima/R Studio/dwd-daten/400_h_0604_0519.csv", header=TRUE, 
-                 na.strings="NA", dec=".", stringsAsFactors = T, sep = ";")
-buch$MESS_DATUM <- as.POSIXct(as.character(buch$MESS_DATUM),format = "%Y%m%d%H",tz="UTC")
-
-#buch$STATIONS_ID <- as.numeric(as.character(buch$STATIONS_ID))
-#buch$RS_IND <- as.numeric(as.character(buch$RS_IND))
-
-pr.b1 <- vector() # empty vector for hourly prcp
-
-for (i in 1:154118) pr.b1[i] <- max(buch[i,3], na.rm=FALSE)
-options(max.print=10000)
-print("")
-print("Hourly")
-print("---------------------------")
-print(round(pr.b1, digits=1)) # output to console (digits = 1 bedeutet dass die Werte auf eine Stelle nach dem Komma gerundet werden)
-
-plot(1:154118, pr.b1, main="Hourly pr (CER 2 km domain)",
-     xlab=paste0("Hours "), ylab="PRCP mm h-1")
+ber <- merge(minhour, ber, by ="MESS_DATUM", all.x = T)
+ber[is.na(ber)] <- 0
+ber[ber == -999] <- 0
