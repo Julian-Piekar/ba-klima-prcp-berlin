550e0b3029ebb53a2032752f067e83be7343326b
Julian-Piekar <124191965+Julian-Piekar@users.noreply.github.com>
2023-02-17 22:12:00 +0100

    Update dwd.R

--- dwd.R
+++ dwd.R
@@ -1,13 +1,18 @@
-# BER = 427
+############################################################################################
+# Buch 2 400
+# Liesst DWD-Daten aus .csv aus, ergänzt fehlende Zeilen/Stunden mit 0, gibt neue vollständige .csv aus
+# Muss für jede Station gemacht werden
 
-ber <- read.csv("C:/UNI/BA/BA Klima/R Studio/dwd-daten/427_h_0104_1219.csv", header=TRUE, 
-               na.strings="NA", dec=".", stringsAsFactors = T, sep = ";")
-ber$MESS_DATUM <- as.POSIXct(as.character(ber$MESS_DATUM),format = "%Y%m%d%H",tz="UTC")
+buch <- read.csv("./dwd-daten/Archive/400_h.csv", header=TRUE, 
+                 na.strings="NA", dec=".", stringsAsFactors = T, sep = ";")
+buch$MESS_DATUM <- as.POSIXct(as.character(buch$MESS_DATUM),format = "%Y%m%d%H",tz="UTC")
+buch <- buch[1:131439,]
 
-minhour <- seq(as.POSIXct("2004-01-01 00:00:00"), as.POSIXct("2004-12-31 23:00:00"), by="hour")
+minhour <- seq(as.POSIXct("2004-06-01 00:00:00"), as.POSIXct("2019-06-01 00:00:00"), by="hour")
 minhour <- data.frame( MESS_DATUM = minhour)
 
+buch <- merge(minhour, buch, by ="MESS_DATUM", all.x = T)
+buch[is.na(buch)] <- 0
+buch[buch == -999] <- 0
 
-ber <- merge(minhour, ber, by ="MESS_DATUM", all.x = T)
-ber[is.na(ber)] <- 0
-ber[ber == -999] <- 0
+write.csv(buch, "./dwd-daten/buch_2_full.csv", row.names = F, quote = F)
